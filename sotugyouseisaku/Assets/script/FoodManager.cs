253af35a89cba2e75610e84464b98e3eb9856dac
oouraissa <issa.ooura13@gmail.com>
2019-11-23 01:06:33 +0900

    病気の死亡条件

--- sotugyouseisaku/Assets/script/FoodManager.cs
+++ sotugyouseisaku/Assets/script/FoodManager.cs
@@ -12,18 +12,25 @@ public class FoodManager : MonoBehaviour
         食中毒,
         糖尿病,
         不眠症,
-        拒食症,
-        心筋症,
+        高血圧,
+        栄養失調,
         ガン,
         肺炎,
         脳卒中
     }
     public float tairyoku;
+    public GameMain gameMain;
     public int foodmoney;
     public Image byoukiImage;
     public Text Listtext;
+    public bool byoukibool;
+    public int deathday;
     private List<Byouki> currentByouki = new List<Byouki>();
 
+    private void Start()
+    {
+        byoukibool = false;
+    }
     /// <summary>
     /// 病気をテキストに記載
     /// </summary>
@@ -35,14 +42,19 @@ public class FoodManager : MonoBehaviour
         }
         else
         {
+            byoukibool = true;
             byoukiImage.gameObject.SetActive(true);
-        }
-
-        foreach(var i in currentByouki)
-        {
-            Listtext.text = i.ToString() + "\n";
-        }
+            foreach (var i in currentByouki)
+            {
+                Listtext.text = i.ToString() + "\n";
+                if (i.ToString() == Byouki.肺炎.ToString())
+                {
+                   
+                }                               
+            }
+        }       
     }
+
     /// <summary>
     /// 体力
     /// </summary>
@@ -55,6 +67,35 @@ public class FoodManager : MonoBehaviour
         }
         return tairyoku;
     }
+
+    public void ByoukiDeath()
+    {
+        if(deathday == 0&&byoukibool==true)
+        {
+            
+        }
+        foreach(var i in currentByouki)//特殊な病気の組み合わせ
+        {
+            int KHS = 0;//風邪、貧血、食中毒のカウント
+            if(i.ToString()==Byouki.脳卒中.ToString())
+            {
+                gameMain.ScneSelect(GameMain.Gamestate.GameOver);//脳卒中は絶対死
+            }
+            else if(i.ToString()==Byouki.風邪.ToString()|| i.ToString() == Byouki.貧血.ToString()||i.ToString() == Byouki.食中毒.ToString())
+            {
+                KHS++;
+                if(KHS==3)
+                {
+                    if(i.ToString()!= Byouki.風邪.ToString() ||i.ToString()!= Byouki.貧血.ToString()|| i.ToString() != Byouki.食中毒.ToString())
+                    {
+                        gameMain.ScneSelect(GameMain.Gamestate.GameOver);//風邪、貧血、食中毒＋何かで死
+                    }
+                }
+            }
+        }
+    }
+
+
 
     /// <summary>
     /// 食べ物の値段
@@ -99,7 +140,7 @@ public class FoodManager : MonoBehaviour
                 currentByouki.Add(Byouki.貧血);
             }
         }
-        else if(currenttairyoku>50)
+        else if(currenttairyoku>50&&foodmoney!=0)
         {
             int random = Random.Range(1, 11);
             if (random == 1)
